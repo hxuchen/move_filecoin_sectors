045ce63ed81a2232707d4a79668ed7b73dcd76a3
apehole <apehole@163.com>
2021-04-10 00:18:19 +0800

    todo

--- cmd/mv_sectors.go
+++ cmd/mv_sectors.go
@@ -1,13 +1,12 @@
 package main
 
 // get mounted directories by type into a list(Support for extended types)
-func initializeMountedDirList()  {
-	
+func initializeMountedDirList() {
+
 }
 
 /*
 	cmd include
 		--src  source location
-		--dst  Transfer target location
+		--dst  Transfer target location(required)
 */
-
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module move_sectors
 
 go 1.15
 
-require github.com/ipfs/go-log v1.0.5 // indirect
+require (
+	github.com/ipfs/go-log v1.0.5
+	github.com/urfave/cli v1.22.5
+)
