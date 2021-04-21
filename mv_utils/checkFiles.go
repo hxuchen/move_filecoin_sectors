9a8a4ede60a5797e64cf34dfa86ed734dcb00d85
snailhuang <snailhuang@163.com>
2021-04-21 21:59:58 +0800

    read the tail of file

--- mv_utils/checkFiles.go
+++ mv_utils/checkFiles.go
@@ -52,10 +52,10 @@ func MakeCalData(filePath string, size int64) ([]byte, error) {
 			// read the tail of file
 			if passed := point + BUFFER_SIZE; passed < size && point+chunk >= size {
 				bufTail := make([]byte, BUFFER_SIZE)
-				if remain := size - (point + BUFFER_SIZE); remain < BUFFER_SIZE {
-					bufTail = make([]byte, remain)
-				}
-				file.Seek(size-int64(len(bufTail)), 0)
+				//if remain := size - (point + BUFFER_SIZE); remain < BUFFER_SIZE {
+				//	bufTail = make([]byte, remain)
+				//}
+				file.Seek(size-BUFFER_SIZE, 0)
 				_, err := file.Read(bufTail)
 				if err != nil && err != io.EOF {
 					return nil, err
