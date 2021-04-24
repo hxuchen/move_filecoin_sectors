e170cc7de813b0770e7deb4b141c6fd232b635ea
snailhuang <snailhuang@163.com>
2021-04-25 00:57:29 +0800

    test 5 ck sha1

--- mv_utils/checkFiles.go
+++ mv_utils/checkFiles.go
@@ -23,7 +23,7 @@ func CalFileSha256(filePath string, size int64) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	return fileMd5(raw)
+	return fileSha1(raw)
 }
 
 func MakeCalData(filePath string, size int64) ([]byte, error) {
