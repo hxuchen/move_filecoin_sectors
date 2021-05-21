591af98ddd9049043fb22d62fe2631fb563eadc8
snailhuang <snailhuang@163.com>
2021-05-21 20:44:53 +0800

    add log

--- cmd/unsealedOperations.go
+++ cmd/unsealedOperations.go
@@ -125,6 +125,7 @@ func (t *UnSealedTask) setStatus(st string) {
 }
 
 func (t *UnSealedTask) startCopy(cfg *Config, dstPathIdxInComp int) {
+	log.Infof("start tp copying %v", *t)
 	// copying unsealed
 	err := copying(t.UnSealedSrc, t.UnSealedDst, cfg.SingleThreadMBPS, cfg.Chunks)
 	if err != nil {
