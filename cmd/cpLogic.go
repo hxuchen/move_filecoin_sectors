919664a6fb09da04dc36bbf3950401444aa15fe7
snailhuang <snailhuang@163.com>
2021-05-22 23:38:12 +0800

    print more detail under env SHOW_DETAIL

--- cmd/cpLogic.go
+++ cmd/cpLogic.go
@@ -227,11 +227,6 @@ func startWork(cfg *Config) {
 		log.Error(err)
 		return
 	}
-	if os.Getenv("SHOW_DETAIL") == "1" {
-		for _, v := range taskListSingleton.Ops {
-			fmt.Println(v.getInfo())
-		}
-	}
 	for {
 		allDone := true
 		for _, v := range taskListSingleton.Ops {
@@ -278,9 +273,6 @@ func startWork(cfg *Config) {
 			fmt.Println("src computer thread info:")
 			for ip, v := range srcComputersMapSingleton.CMap {
 				fmt.Printf("%s: current thread:%d; limit thread:%d \n", ip, v.CurrentThreads, v.LimitThread)
-				for _, p := range v.Paths {
-					fmt.Printf("path: %s,current thread:%d; limit thread:%d \n", p.Location, p.CurrentThreads, p.SinglePathThreadLimit)
-				}
 			}
 
 			fmt.Println("dst computer thread info:")
@@ -289,7 +281,33 @@ func startWork(cfg *Config) {
 				for _, p := range v.Paths {
 					fmt.Printf("path: %s,current thread:%d; limit thread:%d \n", p.Location, p.CurrentThreads, p.SinglePathThreadLimit)
 				}
+				if v.CurrentThreads == 0 {
+					for _, ov := range taskListSingleton.Ops {
+						info := ov.getInfo()
+						if ov.getStatus() != StatusDone {
+							switch fileType {
+							case move_common.Sealed:
+								task := info.(SealedTask)
+								if task.DstIp == v.Ip {
+									fmt.Println(task)
+								}
+							case move_common.Cache:
+								task := info.(CacheTask)
+								if task.DstIp == v.Ip {
+									fmt.Println(task)
+								}
+							case move_common.UnSealed:
+								task := info.(UnSealedTask)
+								if task.DstIp == v.Ip {
+									fmt.Println(task)
+								}
+							}
+						}
+
+					}
+				}
 			}
+
 		}
 		time.Sleep(time.Second * 5)
 	}
